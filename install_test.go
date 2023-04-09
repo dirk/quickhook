2ed19b87955435c4ae3127f60049742663b20dd8
Dirk Gadsden <dirk@esherido.com>
2023-04-09 15:17:41 -0700

    Add test coverage for `quickhook install` command

---
+++ install_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dirk/quickhook/testutils"
+)
+
+func TestInstallPreCommitYes(t *testing.T) {
+	tempDir := testutils.NewTempDir(t, 0)
+	tempDir.RequireExec("git", "init", "--quiet", ".")
+	tempDir.MkdirAll(".quickhook", "pre-commit")
+
+	output, err := tempDir.ExecQuickhook("install", "--yes")
+	assert.NoError(t, err)
+	shimPath := path.Join(".git", "hooks", "pre-commit")
+	assert.Equal(t,
+		fmt.Sprintf("Installed shim %v", shimPath),
+		strings.TrimSpace(output))
+	assert.FileExists(t,
+		path.Join(tempDir.Root, shimPath))
+}
---
+++ testutils/tempdir.go
@@ -0,0 +1,69 @@
+package testutils
+
+import (
+	"os"
+	"os/exec"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type TempDir struct {
+	t         *testing.T
+	Root      string
+	Quickhook string
+}
+
+// Depth should be the depth of the tests from the package root: this is needed
+// to correctly find the built quickhook binary for integration testing.
+func NewTempDir(t *testing.T, depth int) TempDir {
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+
+	elem := []string{cwd}
+	for i := 0; i < depth; i++ {
+		elem = append(elem, "..")
+	}
+	elem = append(elem, "quickhook")
+
+	return TempDir{
+		t:         t,
+		Root:      t.TempDir(),
+		Quickhook: path.Join(elem...),
+	}
+}
+
+func (tempDir *TempDir) NewCommand(name string, arg ...string) *exec.Cmd {
+	cmd := exec.Command(name, arg...)
+	cmd.Dir = tempDir.Root
+	return cmd
+}
+
+func (tempDir *TempDir) RequireExec(name string, arg ...string) {
+	cmd := tempDir.NewCommand(name, arg...)
+	_, err := cmd.Output()
+	require.NoError(tempDir.t, err, cmd)
+}
+
+func (tempDir *TempDir) ExecQuickhook(arg ...string) (string, error) {
+	cmd := tempDir.NewCommand(tempDir.Quickhook, arg...)
+	output, err := cmd.CombinedOutput()
+	return string(output), err
+}
+
+func (tempDir *TempDir) WriteFile(relativePath []string, data string) {
+	fullPath := path.Join(append([]string{tempDir.Root}, relativePath...)...)
+	err := os.WriteFile(fullPath, []byte(data), 0755)
+	if err != nil {
+		tempDir.t.Fatal(err)
+	}
+}
+
+func (tempDir *TempDir) MkdirAll(relativePath ...string) {
+	fullPath := path.Join(append([]string{tempDir.Root}, relativePath...)...)
+	err := os.MkdirAll(fullPath, 0755)
+	if err != nil {
+		tempDir.t.Fatal(err)
+	}
+}
