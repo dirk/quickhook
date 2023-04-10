2e309b4e64f6d77e7d95aa48e453222fc9da49d8
Dirk Gadsden <dirk@esherido.com>
2023-04-09 21:11:23 -0700

    Combine integration and unit test coverage (#13)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,19 +3,29 @@
 set -e
 
 # Run this from the repository root, not from scripts!
-export GOCOVERDIR=$(pwd)/coverage
+
+COVERAGE=$(pwd)/coverage
+INTEGRATION_COVERAGE=$COVERAGE/integration
 
 echo "Rebuilding with coverage enabled..."
 go clean
 go build -cover
 
-echo "Running tests with coverage written to $GOCOVERDIR..."
-rm -rf $GOCOVERDIR
-mkdir $GOCOVERDIR
-go test ./... -count=1
+rm -rf $COVERAGE
+mkdir -p $INTEGRATION_COVERAGE
 
-echo "Converting coverage formats..."
-go tool covdata textfmt -i=$GOCOVERDIR -o $GOCOVERDIR/coverage.txt
+echo "Running integration tests with coverage written to $INTEGRATION_COVERAGE..."
+GOCOVERDIR=$INTEGRATION_COVERAGE go test ./... -count=1
+
+echo "Running unit tests with coverage written to $UNIT_COVERAGE..."
+go test ./... -count=1 -coverprofile=$COVERAGE/unit-coverage.txt
+
+echo "Converting integration coverage format..."
+go tool covdata textfmt -i=$INTEGRATION_COVERAGE -o $COVERAGE/integration-coverage.txt
+
+echo "Merging coverage..."
+cp $COVERAGE/integration-coverage.txt $COVERAGE/coverage.txt
+tail -n +2 $COVERAGE/unit-coverage.txt >> $COVERAGE/coverage.txt
 
 echo "Opening coverage..."
-go tool cover -html=$GOCOVERDIR/coverage.txt
+go tool cover -html=$COVERAGE/coverage.txt
