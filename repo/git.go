c108868d48520c74e506b0f6faa91f80de83a104
Dirk Gadsden <dirk@esherido.com>
2023-04-10 19:08:39 -0700

    Run pre-commit hook's preamble steps in parallel (#17)
    The preamble steps in the pre-commit hook (`git diff`, discovering
    the list of hook executables, shimming Git) are all IO-bound, so we
    can run them in parallel.

--- repo/git.go
+++ repo/git.go
@@ -1,10 +1,6 @@
 package repo
 
 import (
-	"os"
-	"path"
-	"strings"
-
 	"github.com/samber/lo"
 
 	"github.com/dirk/quickhook/tracing"
@@ -21,27 +17,4 @@ func (repo *Repo) FilesToBeCommitted() ([]string, error) {
 		isFile, _ := repo.isFile(line)
 		return isFile
 	}), err
-}
-
-func (repo *Repo) ShimGit() (string, error) {
-	span := tracing.NewSpan("git shim")
-	defer span.End()
-
-	dir, err := os.MkdirTemp("", "quickhook-git-*")
-	if err != nil {
-		return "", err
-	}
-
-	git := path.Join(dir, "git")
-	err = os.WriteFile(git, []byte(strings.Join([]string{
-		"#!/bin/sh",
-		"echo \"git is not allowed in parallel hooks (git $@)\"",
-		"exit 1",
-		"",
-	}, "\n")), 0755)
-	if err != nil {
-		return "", err
-	}
-
-	return dir, nil
 }
